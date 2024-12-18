5ee1a60c660138ad40d97b42a46168067916e549
ilmi-id <ujiakunweb@gmail.com>
2024-12-18 22:31:49 +0800

    u: index.blade.php

--- resources/views/Beranda/index.blade.php
+++ resources/views/Beranda/index.blade.php
@@ -52,7 +52,7 @@ https://templatemo.com/tm-562-space-dynamic
           <nav class="main-nav">
             <!-- ***** Logo Start ***** -->
             <a href="index.html" class="logo">
-              <h4>Spac<span>Dyna</span></h4>
+              <h4>Bea<span>pintar</span></h4>
             </a>
             <!-- ***** Logo End ***** -->
             <!-- ***** Menu Start ***** -->
@@ -83,17 +83,9 @@ https://templatemo.com/tm-562-space-dynamic
           <div class="row">
             <div class="col-lg-6 align-self-center">
               <div class="left-content header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
-                <h6>Welcome to Space Dynamic</h6>
-                <h2>We Make <em>Digital Ideas</em> &amp; <span>SEO</span> Marketing</h2>
-                <p>Space Dynamic is a professional looking HTML template using a Bootstrap 5 (beta 2). This CSS template is free for you provided by <a rel="nofollow" href="https://templatemo.com/page/1" target="_parent">TemplateMo</a>.</p>
-                <form id="search" action="#" method="GET">
-                  <fieldset>
-                    <input type="address" name="address" class="email" placeholder="Your website URL..." autocomplete="on" required>
-                  </fieldset>
-                  <fieldset>
-                    <button type="submit" class="main-button">Analyze Site</button>
-                  </fieldset>
-                </form>
+                <h6>Selamat Datang di BeaPintar</h6>
+                <h2>Meraih <em>Pendidikan</em> &amp; <span>Membangun</span> Masa Depan</h2>
+                <p>Temukan peluang beasiswa yang membawa harapan baru bagi pendidikan Anda. Wujudkan impian dengan kami.</p>
               </div>
             </div>
             <div class="col-lg-6">
